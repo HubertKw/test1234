03ef02803626684ecf4af6fe638f321c34064762
hubert.kwinta <hubertkwinta01@gmail.com>
2024-04-23 17:44:27 +0200

    testb

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Heldlo World!\n";
+    std::cout << "Heaefeldlo World!\n";
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
